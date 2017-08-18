5790c50bf0832f3d7cb9a3cc557b95fed3dd8423
radionov <boss@flagstudio.ru>
2017-08-18 13:11:36 +0500

    sudo remove

--- acf.sh
+++ acf.sh
@@ -3,14 +3,14 @@ if [ $sys == 'Darwin' ];
 
 then
     #Mac OS
-  sudo sed -i '' s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g plugins/advanced-custom-fields-pro/api/api-helpers.php
+  sed -i '' s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g plugins/advanced-custom-fields-pro/api/api-helpers.php
 
 else
   #не Mac OS
-  sudo sed -i s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g plugins/advanced-custom-fields-pro/api/api-helpers.php
+  sed -i s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g plugins/advanced-custom-fields-pro/api/api-helpers.php
 
 fi
 
-sudo mkdir themes/template/acf-json
-sudo chmod 777 themes/template/acf-json
-sudo echo '<? echo "Silence is golden";?>'>>themes/template/acf-json/index.php
+mkdir themes/template/acf-json
+chmod 777 themes/template/acf-json
+echo '<? echo "Silence is golden";?>'>>themes/template/acf-json/index.php
