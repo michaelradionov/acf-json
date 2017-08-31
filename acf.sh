527a3049ae87bfa78048a7ef2eb04b488ec60cbb
radionov <boss@flagstudio.ru>
2017-08-31 16:51:36 +0500

    change root path

--- acf.sh
+++ acf.sh
@@ -5,15 +5,15 @@ if [ $sys == 'Darwin' ];
 
 then
     #Mac OS
-  sed -i '' s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g plugins/advanced-custom-fields-pro/api/api-helpers.php
+  sed -i '' s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g ../../plugins/advanced-custom-fields-pro/api/api-helpers.php
 
 else
   #не Mac OS
-  sed -i s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g plugins/advanced-custom-fields-pro/api/api-helpers.php
+  sed -i s/'return json_encode($json, JSON_PRETTY_PRINT)'/'return json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)'/g ../../plugins/advanced-custom-fields-pro/api/api-helpers.php
 
 fi
 
-mkdir themes/template/acf-json
-chmod 777 themes/template/acf-json
-echo '<? echo "Silence is golden";?>'>themes/template/acf-json/index.php
+mkdir acf-json
+chmod 777 acf-json
+echo '<? echo "Silence is golden";?>'>acf-json/index.php
 rm acf.sh
